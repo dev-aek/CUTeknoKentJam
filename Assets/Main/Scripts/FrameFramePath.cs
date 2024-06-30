d7c637aabe87b49b67a0c16cd56e6e4224a9952d
Abdullah Enes Kocabuğa <75130813+dev-aek@users.noreply.github.com>
2024-06-30 08:45:48 +0300

    ADD: Hologram Smooths with DotWeen

--- Assets/Main/Scripts/FrameFramePath.cs
+++ Assets/Main/Scripts/FrameFramePath.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class FrameFramePath : MonoBehaviour
 {
@@ -16,18 +17,47 @@ public class FrameFramePath : MonoBehaviour
     {
         
             // Nesneleri tek tek aç
-            foreach (GameObject obj in objectsToActivate)
+           /* foreach (GameObject obj in objectsToActivate)
             {
                 obj.SetActive(true);
                 yield return new WaitForSeconds(delay);
-            }
+            }*/
 
-            // Nesneleri tek tek kapat
-            foreach (GameObject obj in objectsToActivate)
+        for (int i = 0; i < objectsToActivate.Count; i++)
+        {
+            objectsToActivate[i].SetActive(true);
+            if (i>0)
+            {
+                Vector3 pos = objectsToActivate[i].transform.position;
+                objectsToActivate[i].transform.position = objectsToActivate[i - 1].transform.position;
+                objectsToActivate[i].transform.DOMove(pos, delay-0.05f);
+            }
+            yield return new WaitForSeconds(delay);
+        }
+
+        for (int i = 0; i < objectsToActivate.Count; i++)
+        {
+            Vector3 pos = objectsToActivate[i].transform.position;
+            if (i < objectsToActivate.Count-1)
+            {
+                Vector3 nextPos= objectsToActivate[i + 1].transform.position;
+                objectsToActivate[i].transform.DOMove(nextPos, delay - 0.05f).OnComplete(()=> objectsToActivate[i].SetActive(false));
+            }
+            else
+            {
+                objectsToActivate[i].SetActive(false);
+            }
+            yield return new WaitForSeconds(delay);
+            objectsToActivate[i].transform.position = pos;
+
+        }
+
+        // Nesneleri tek tek kapat
+        /*foreach (GameObject obj in objectsToActivate)
             {
                 yield return new WaitForSeconds(delay);
                 obj.SetActive(false);
-            }
+            }*/
         StartCoroutine(LoopActivationDeactivation());
     }
 
